6058ec493ba137faf65ff0f42beec421349a3354
StrungPattern-coder <ksriram4584@gmail.com>
2025-01-07 02:48:52 +0530

    Server end changes

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const authenticateJWT = (req, res, next) => {
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error(err));
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 // Routes
 // 1. Admin Login
